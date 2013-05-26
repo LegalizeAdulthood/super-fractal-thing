8091d9fe753950ad16fe7af66b572c63edfaf8f2
Kevin Martin <superfractalthing@gmail.com>
2013-05-26 19:40:10 +0100

    Add gpl comment

--- SuperFractalThing/source/SftComponent.java
+++ SuperFractalThing/source/SftComponent.java
@@ -1,3 +1,24 @@
+//	SFTComponent
+//
+//
+//    Copyright 2013 Kevin Martin
+//
+//    This file is part of SuperFractalThing.
+//
+//    SuperFractalThing is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    any later version.
+//
+//    SuperFractalThing is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with SuperFractalThing.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
