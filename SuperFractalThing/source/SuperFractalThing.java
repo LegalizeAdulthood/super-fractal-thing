0be564a64dc11ce7c5bfd7418cfc101bc3d45636
Kevin Martin <superfractalthing@gmail.com>
2015-04-18 12:45:44 +0100

    Fix for locale errors by by Mikko Tommila

--- SuperFractalThing/source/SuperFractalThing.java
+++ SuperFractalThing/source/SuperFractalThing.java
@@ -189,7 +189,7 @@ public class SuperFractalThing  extends JApplet implements SFTGui, ActionListene
 			str="s="+half_size.toString()+"\n";
 			str+="r="+mPos_x_box.getText()+"\n";
 			str+="i="+mPos_y_box.getText()+"\n";
-			str+="iteration_limit="+mIterations_box.getText().replaceAll(",", "")+"\n";
+			str+="iteration_limit="+mIterations_box.getValue()+"\n";
 
 			
 			SaveFile(str);
@@ -475,14 +475,10 @@ public class SuperFractalThing  extends JApplet implements SFTGui, ActionListene
 	}
 	public int GetIterations()
 	{
-		String str = mIterations_box.getText().replaceAll(",", "");
-		str = str.replaceAll(" ", "");
-		str = str.replaceAll("\\.", "");
-		return Integer.parseInt(str);
+		return ((Number) mIterations_box.getValue()).intValue();
 	}
 	public void SetIterations(int aValue)
 	{
-//		mIterations_box.setText(Integer.toString(aValue));
 		mIterations_box.setValue(aValue);
 	}
 	public BigDecimal[] GetCoords()
